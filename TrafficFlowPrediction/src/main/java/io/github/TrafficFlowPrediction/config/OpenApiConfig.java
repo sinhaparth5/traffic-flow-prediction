4ff41b11edfed0af67328bc71f219465b71c5907
Parth Sinha <sinhaparth555@gmail.com>
2025-02-15 14:24:08 +0000

    added swagger ui

--- TrafficFlowPrediction/src/main/java/io/github/TrafficFlowPrediction/config/OpenApiConfig.java
+++ TrafficFlowPrediction/src/main/java/io/github/TrafficFlowPrediction/config/OpenApiConfig.java
@@ -5,10 +5,32 @@
 
 package io.github.TrafficFlowPrediction.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
 /**
  *
  * @author parth
  */
+
+ @Configuration
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Traffic Flow Prediction API")
+                .version("1.0")
+                .description("API for Traffic Flow Prediction")
+                .contact(new Contact()
+                    .name("Parth Sinha")
+                    .email("parth_sinha@yahoo.com")
+                )
+            );
+    }
 }
