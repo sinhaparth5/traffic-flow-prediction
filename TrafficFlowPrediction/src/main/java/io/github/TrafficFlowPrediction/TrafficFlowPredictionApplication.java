a20ebf9bc218cb363ebfdc4ce87996d4b98adeb1
Parth Sinha <sinhaparth555@gmail.com>
2025-02-15 13:35:25 +0000

    added backend and frontend

---
+++ TrafficFlowPrediction/src/main/java/io/github/TrafficFlowPrediction/TrafficFlowPredictionApplication.java
@@ -0,0 +1,13 @@
+package io.github.TrafficFlowPrediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficFlowPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficFlowPredictionApplication.class, args);
+	}
+
+}
---
+++ TrafficFlowPrediction/src/test/java/io/github/TrafficFlowPrediction/TrafficFlowPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.TrafficFlowPrediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficFlowPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
